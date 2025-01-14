82ff41467b6eb113e502711f056f6da13fc56666
rexmarlon <34959133+rexmarlon@users.noreply.github.com>
2025-01-14 12:35:32 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -128,8 +128,7 @@ def post_thread(whitepaper_content):
                     time.sleep(5)  # Add a delay between tweets to avoid rate limits
                     break
                 except tweepy.errors.TooManyRequests:
-                    print("Rate limit reached. Sleeping for 15 minutes.")
-                    time.sleep(900)  # Sleep for 15 minutes
+                    print("Rate limit reached.) 
                 except Exception as e:
                     print(f"Error posting tweet {i+1}: {e}")
                     if attempt < 2:
